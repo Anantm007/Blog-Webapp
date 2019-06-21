71b898007ea77354238f54e4544ce2dfaf6ec30e
Anant Mathur <anant.mathur007@gmail.com>
2019-06-21 16:54:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Hey guys! This is Anant, a self taught web developer. I have made this blog from scratch using modern tool and technologies such as HTML5, CSS3, Bootstrap, Node.js, Express.js and MongoDB database";
+const aboutContent = "I am a Computer Science student skilled in HTML, CSS, Bootstrap, Javascript, jQuery, Node.js, Express.js, MongoDB, C, C++ and Python. Competitive coding and web development interest me. I am a resident of New Delhi, India and looking for internship opportunities to further enhance my knowledge and skills.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-Anant:test123@cluster1-r8m7j.mongodb.net/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started succcessfully");
+});
